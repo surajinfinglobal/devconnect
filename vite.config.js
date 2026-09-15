63c5db79608b8ec37abf13c09a5cb09767226c6c
IG-06 <suraj.infinglobal@gmail.com>
2026-09-15 17:04:56 +0530

    fix Error

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,21 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+
   resolve: {
     alias: {
-      '@': path.resolve(import.meta.dirname, './src'),
-      '@/components': path.resolve(import.meta.dirname, './components'),
-      '@/components/ui/smoothui': path.resolve(import.meta.dirname, './components/ui/smoothui'),
-      '@/components/smoothui': path.resolve(import.meta.dirname, './components/ui/smoothui'),
+      '@': path.resolve(__dirname, './src'),
+      '@/components': path.resolve(__dirname, './components'),
+      '@/components/ui/smoothui': path.resolve(__dirname, './components/ui/smoothui'),
+      '@/components/smoothui': path.resolve(__dirname, './components/ui/smoothui'),
     },
   },
 })
