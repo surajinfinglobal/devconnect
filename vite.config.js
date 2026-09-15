28b3cf4609b9c81a0571c20e79fc44a00395a3dc
IG-06 <suraj.infinglobal@gmail.com>
2026-09-15 17:36:01 +0530

    Fix Vite path resolution

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,6 @@ const __dirname = path.dirname(__filename)
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
